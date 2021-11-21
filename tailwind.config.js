8600ed3f772291b2cf09d92d4882e7bb07f2bbfb
Adjie DJaka Permana <adjie.djaka@ui.ac.id>
2021-11-22 06:08:59 +0700

    css setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue-solomon': '#07BCBC',
+        'orange-solomon': '#FFC440',
+      },
+      backgroundImage: {
+        'hero-pattern': "url('../images/hero_bg.svg')",
+      },
+    },
   },
   variants: {
     extend: {},
