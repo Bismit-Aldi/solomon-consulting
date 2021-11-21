bc4388f9da4e6234130ddd7cefa638abad3cbb77
Adjie DJaka Permana <adjie.djaka@ui.ac.id>
2021-11-22 06:08:29 +0700

    removed boilerplate, changed to jsx, & setup route

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 
 import ReactDOM from 'react-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
-
-import '~slick-carousel/slick/slick.css';
-
-import App from './App';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './assets/css/index.css';
 
 library.add(fab, fas, far);
 
