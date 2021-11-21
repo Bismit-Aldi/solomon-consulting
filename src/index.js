d664b8a2dd0e920ecb2de5f7741b4925cd947fce
Adjie DJaka Permana <adjie.djaka@ui.ac.id>
2021-11-21 22:43:17 +0700

    INITIAL COMMIT

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
-import './index.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import '~slick-carousel/slick/slick.css';
+
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+library.add(fab, fas, far);
 
 ReactDOM.render(
   <React.StrictMode>
