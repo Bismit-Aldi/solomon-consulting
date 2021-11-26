53f8891f2e9ba329d8ddfeffc179e305883e696b
Adjie DJaka Permana <adjie.djaka@ui.ac.id>
2021-11-26 19:55:52 +0700

    added dynamic color

--- src/components/global/HeadTitle.jsx
+++ src/components/global/HeadTitle.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const HeadTitle = ({ title }) => {
+const HeadTitle = ({ title, color }) => {
   return (
     <h1 className='flex flex-col item-center'>
-      <span className='text-2xl font-bold text-white'>{title}</span>
+      <span className={`text-2xl font-bold ${color}`}>{title}</span>
       <div className='w-full h-2 rounded bg-orange-solomon'></div>
     </h1>
   );
